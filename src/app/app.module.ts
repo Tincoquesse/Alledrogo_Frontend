6a6d210661238a3812032e8717b3a6d6a0bcedb1
Tincoquesse <kamil.sound@gmail.com>
2022-06-21 13:25:00 +0200

    alledrogo-products.module.ts added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {AlledrogoProductsModule} from "./alledrogo-products/alledrogo-products.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AlledrogoProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
