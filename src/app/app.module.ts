bc10389fc91883347a0a2a590e0f14cad5224a4d
Tincoquesse <kamil.sound@gmail.com>
2022-08-16 14:20:39 +0200

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {AlledrogoProductsModule} from "./alledrogo-products/alledrogo-products.module";
 import {FormsModule} from "@angular/forms";
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 import {SharedModule} from "./shared/shared.module";
 import {AlledrogoBasketModule} from "./alledrogo-basket/alledrogo-basket.module";
 import {AlledrogoLoginModule} from "./alledrogo-login/alledrogo-login.module";
-import { JwtModule } from "@auth0/angular-jwt";
+import {JwtModule} from "@auth0/angular-jwt";
 import {AlledrogoRegisterModule} from "./alledrogo-register/alledrogo-register.module";
 import {AuthModule} from "./auth/auth.module";
 import {AuthGuard} from "./auth/services/auth.guard";
@@ -45,8 +45,6 @@ export function tokenGetter() {
         throwNoTokenError: true,
       },
     }),
-
-
   ],
   providers: [AuthGuard],
   bootstrap: [AppComponent]
